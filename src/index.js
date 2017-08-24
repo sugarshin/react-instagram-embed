c01830536f4e3d2c046bbbaaa7d6724febf07e52
Timofey Bochkarev <t.bochkarev@go-promo.ru>
2017-08-24 12:41:14 +0300

    ADDED canceling request in componentWillUnmount

--- src/index.js
+++ src/index.js
@@ -80,6 +80,10 @@ export default class InstagramEmbed extends Component {
     return <this.props.containerTagName {...this.omitComponentProps()} dangerouslySetInnerHTML={{ __html: this.state.__html }} />
   }
 
+  componentWillUnmount() {
+    this.cancel()
+  }
+
   omitComponentProps(): Object {
     // eslint-disable-next-line no-unused-vars
     const { url, hideCaption, maxWidth, containerTagName, onLoading, onSuccess, onAfterRender, onFailure, protocol, ...rest } = this.props
